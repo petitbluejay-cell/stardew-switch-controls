82d7594c894faa20a301a80e4bcc99cf143bfa47
Brian Kendall <brian@briankendall.net>
2024-11-16 19:05:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is just a simple mod that changes up desktop Stardew Valley's controls to match the controls on the Switch version, for those of us who originally played the game on Switch and don't particularly feel like rewiring their muscle memory.
+
+It works better than just using something like Steam or another app to switch the A and B buttons on your controller, because with this mod the controls will still work properly when playing "Journey of the Prairie King". It also helps in those situations where there's no way to change up the controller mapping anyway. (I'm looking at you and your non-functional "Game Controller" system settings, macOS.)
+
+There's no configuration for this mod. All you need is SMAPI and then to copy it into your mods folder. It's tested in Stardew Valley 1.6.14, and will hopefully work in later versions as well.
---
+++ SwitchControls.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using Microsoft.Xna.Framework.Input;
+using HarmonyLib;
+using System.Reflection;
+
+namespace SwitchControls
+{
+    internal class ObjectPatches
+    {
+        private static IMonitor? Monitor = null;
+
+        // call this method from your Entry class
+        internal static void Initialize(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
+        
+        internal static void UpdateStates_postfix(StardewValley.InputState __instance)
+        {
+            try
+            {
+                if (StardewValley.Game1.currentMinigame is StardewValley.Minigames.AbigailGame) {
+                    return;
+                }
+                
+                Traverse field = Traverse.Create(__instance).Field("_currentGamepadState");
+                GamePadState state = field.GetValue<GamePadState>();
+                
+                GamePadButtons buttonState = state.Buttons;
+                Buttons orgButtons = Traverse.Create(buttonState).Field("_buttons").GetValue<Buttons>();
+                Buttons newButtons = orgButtons;
+                
+                if ((orgButtons & Buttons.A) == Buttons.A) {
+                    newButtons &= (~Buttons.A);
+                    newButtons |= (Buttons.B);
+                }
+                
+                if ((orgButtons & Buttons.B) == Buttons.B) {
+                    newButtons &= (~Buttons.B);
+                    newButtons |= (Buttons.A);
+                }
+                
+                GamePadState newState = new GamePadState(
+                    state.ThumbSticks,
+                    state.Triggers,
+                    new GamePadButtons(newButtons),
+                    state.DPad
+                );
+                
+                field.SetValue(newState);
+            }
+            catch (Exception ex)
+            {
+                if (Monitor != null) {
+                    Monitor.Log($"Failed in {nameof(UpdateStates_postfix)}:\n{ex}", LogLevel.Error);
+                }
+            }
+        }
+    }
+    
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ObjectPatches.Initialize(this.Monitor);
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.InputState), nameof(StardewValley.InputState.UpdateStates)),
+               postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.UpdateStates_postfix))
+            );
+        }
+    }
+}
